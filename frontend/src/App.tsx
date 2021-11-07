8e7588d31d8f4e61815994be7aadda3184232b7f
Daniel Soares <danielsoares321@gmail.com>
2021-11-07 02:11:13 -0300

    Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import Datatable from "components/Datatable";
+import Footer from "components/footer";
+import Navbar from "components/navbar";
 
 function App() {
   return (
-    <div>
+    <>
+    <Navbar />
+    <div className="container">
       <h1 className="text-primary">ola mundo!</h1>
+      <Datatable />
       </div>
+      <Footer />
+      </>
   );
 }
 
